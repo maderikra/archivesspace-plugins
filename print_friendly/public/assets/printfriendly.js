4114eeed2b2f9a1efc9a9f1c7b065a3e6d800c10
maderikra <maderikra@vmi.edu>
2016-11-16 14:36:24 -0500

    Update printfriendly.js

--- print_friendly/public/assets/printfriendly.js
+++ print_friendly/public/assets/printfriendly.js
@@ -5,10 +5,10 @@ $( ".content-pane .row-fluid .span9" ).attr( "id", "print_content" );
 
 
           //adds print link if it doesn't already exist
-          if ($("#printlink").length == 0){
-           $( ".title-pane .row-fluid .span3").append( '<br /><div id=\"printlink\" style=\"text-align:center\"><img src=\"/assets/printer_icon.png\"><a href=\"#\" onclick=\"window.open(\'/assets/print.html$
-          }
-
+     if ($("#printlink").length == 0){
+	   $( ".title-pane .row-fluid .span3").append( '<br /><div id=\"printlink\" style=\"text-align:center\"><img src=\"/assets/printer_icon.png\"><a href=\"#\" onclick=\"window.open(\'/assets/print.html\')\">Print Friendly</a></div>' );
+	  }
+	
 
 
         });
